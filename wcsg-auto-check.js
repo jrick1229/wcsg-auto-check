9e6ea7a25a2c1af477396fcc147523cdf41980e5
jrick1229 <joey@prospress.com>
2018-10-22 14:55:41 -0400

    remove on-load

--- wcsg-auto-check.js
+++ wcsg-auto-check.js
@@ -1,6 +1,4 @@
 jQuery(document).ready(function($) {
-    $(window).on("load", function(){
-        $('input#gifting_0_option').prop("checked", true);
-        $('p.woocommerce_subscriptions_gifting_recipient_email').show();
-    });
+    $('input#gifting_0_option').prop("checked", true);
+    $('p.woocommerce_subscriptions_gifting_recipient_email').show();
 });
